f8c2a34b4feaffafb6a7822bfa40e3e73cadeb1f
XaiaX <xaiax@xaiax.net>
2026-08-05 18:17:24 -0700

    test: inspect expanded Maestro prefab

--- Assets/Tests/EditMode/MaestroEditModeSmoke.cs
+++ Assets/Tests/EditMode/MaestroEditModeSmoke.cs
@@ -432,12 +432,21 @@ namespace YARG.Tests.EditMode
                 "Maestro must contain the shared Header prefab instance.");
             Assert.That(prefabText, Does.Contain(
                 "guid: fbe721481a76d3340871db2a026bbbcb"));
-            var hierarchyNames = prefab.GetComponentsInChildren<Transform>(true)
-                .Select(transform => transform.name)
-                .ToArray();
-            Assert.That(hierarchyNames, Does.Contain("SharedHeader"));
-            Assert.That(hierarchyNames, Does.Contain("Single Header Text"));
-            Assert.That(hierarchyNames, Does.Contain("Button"));
+            var prefabContents = PrefabUtility.LoadPrefabContents(
+                "Assets/Prefabs/Menu/Maestro/MaestroSetupMenu.prefab");
+            try
+            {
+                var hierarchyNames = prefabContents.GetComponentsInChildren<Transform>(true)
+                    .Select(transform => transform.name)
+                    .ToArray();
+                Assert.That(hierarchyNames, Does.Contain("SharedHeader"));
+                Assert.That(hierarchyNames, Does.Contain("Single Header Text"));
+                Assert.That(hierarchyNames, Does.Contain("Button"));
+            }
+            finally
+            {
+                PrefabUtility.UnloadPrefabContents(prefabContents);
+            }
         }
 
         [Test]
