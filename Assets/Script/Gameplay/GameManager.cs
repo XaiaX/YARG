d3621f89e1cabebc53d40394d25b975b26f38795
XaiaX <xaiax@xaiax.net>
2026-08-09 10:23:08 -0700

    fix: preserve song speed when force quitting

--- Assets/Script/Gameplay/GameManager.cs
+++ Assets/Script/Gameplay/GameManager.cs
@@ -800,7 +800,9 @@ namespace YARG.Gameplay
 
         public void ForceQuitSong()
         {
+            float songSpeed = GlobalVariables.State.SongSpeed;
             GlobalVariables.State = PersistentState.Default;
+            GlobalVariables.State.SongSpeed = songSpeed;
             GlobalVariables.Instance.LoadScene(SceneIndex.Menu);
         }
 
